8b503ed7f9e7b3ea6813b21539d089cbc913979a
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-05-02 19:27:21 -0300

    Classe DAOCategoria adicionando os metodos de incluir e alterar

---
+++ DAO/DAOCategoria.cs
@@ -0,0 +1,43 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO {
+    class DAOCategoria {
+
+        // ATRIBUTOS
+        public DAOConexao daoConexao;
+
+        // CONSTRUTOR
+        public DAOCategoria(DAOConexao conexao){
+            daoConexao = conexao;
+        }
+
+        // INCLUIR NO BANCO
+        public void incluir(ModeloCategoria mCategoria){
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = daoConexao.Conexao;
+            cmd.CommandText = "insert into categoria(cat_nome) values(@nome); select @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@nome", mCategoria.CatNome);
+            daoConexao.conectar();
+            mCategoria.CatCod = Convert.ToInt32(cmd.ExecuteScalar());
+            daoConexao.desconectar();
+        }
+
+        // ALTERA NO BANCO
+        public void alterar(ModeloCategoria mCategoria) {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = daoConexao.Conexao;
+            cmd.CommandText = "update categoria set cat_nome =  @nome where cat_cod = @codigo;";
+            cmd.Parameters.AddWithValue("@nome", mCategoria.CatNome);
+            cmd.Parameters.AddWithValue("@codigo", mCategoria.CatCod);
+            daoConexao.conectar();
+            cmd.ExecuteNonQuery();
+            daoConexao.desconectar();
+        }
+    }
+}
---
+++ DAO/DAOConexao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO {
+    class DAOConexao {
+
+        // ATRIBUTROS
+        private string _stringConexao;
+        private SqlConnection _conexao;
+
+        // CONSTRUTOR QUE RECEBE UMA STRING DE CONEXAO
+        public DAOConexao(string dadosConexao){
+            _conexao = new SqlConnection();
+            StringConexao = dadosConexao;
+            _conexao.ConnectionString = dadosConexao;
+        }
+
+        // ENCAPSULAMENTO
+        public String StringConexao{
+          get { return _stringConexao; }
+          set { _stringConexao = value; }
+        }
+        public SqlConnection Conexao{
+          get { return _conexao; }
+          set { _conexao = value; }
+        }
+
+        // CONECTAR AO BANCO
+        public void conectar(){
+            _conexao.Open();
+        }
+
+        // DESCONECTAR DO BANCO
+        public void desconectar(){
+            _conexao.Close();
+        }
+    }
+}
