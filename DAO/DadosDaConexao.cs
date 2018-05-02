5dd5fd9eb137d51de7c414b65b35f5b628ad401b
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-05-02 18:51:21 -0300

    Iniciando o projeto e criando a conexao com o banco de dados

---
+++ DAO/DadosDaConexao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO {
+    class DadosDaConexao {
+
+      // CRIA A STRING DE CONEXAO
+      public static String StringDeConexao{
+        get {
+                return "Data Source=EDUARDO-PC\\SQLEXPRESS;Initial Catalog=BDESTOQUE;Integrated Security=True";
+        }
+      }
+    }
+}
---
+++ Modelo/ModeloCategoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo {
+    public class ModeloCategoria {
+
+        // ATRIBUTOS
+        private int cat_cod;
+        private string cat_nome;
+
+        // ENCAPSULAMENTO
+        public int CatCod{
+          get{ return cat_cod; }
+          set{ cat_cod = value; }
+        }
+        public String CatNome{
+          get{ return cat_nome; }
+          set{ cat_nome = value; }
+        }
+    }
+}
